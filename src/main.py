881d7ea14ad45b3fc5221ebb1b6f54bd3b7d59e9
albertgoncalves <goncalves.albert@gmail.com>
2019-08-25 16:10:47 -0400

    test line intersection

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@
 
 from operator import lt
 from os import environ
-from random import randint, seed
+from random import seed, shuffle
 
 from matplotlib.pyplot import close, savefig, subplots, tight_layout
 
 from algo import convex_hull
 from bst import insert, leaf, show
-from gen import random_points
+from gen import random_lines, random_points
+from geom import point_of_intersection
+
+WD = environ["WD"]
 
 
 def init_plot():
@@ -26,40 +29,44 @@ def plot_lines(ax, lines):
         ax.plot(*zip(*line), zorder=0)
 
 
-def save_plot(filename):
+def export(filename):
     tight_layout()
     savefig(filename)
     close()
-
-
-def plot_convex_hull(points, filename):
-    ax = init_plot()
-    plot_points(ax, points)
-    plot_lines(ax, convex_hull(points))
-    save_plot(filename)
 
 
 def main():
     def demo_convex_hulls(n):
         for i in range(n):
             seed(i)
-            plot_convex_hull(
-                random_points(50),
-                "{}/out/convex_hull_{}.png".format(environ["WD"], i),
-            )
+            points = random_points(50)
+            ax = init_plot()
+            plot_points(ax, points)
+            plot_lines(ax, convex_hull(points))
+            export("{}/out/convex_hull_{}.png".format(WD, i))
+
+    def demo_point_of_intersection(n):
+        for i in range(n):
+            lines = random_lines(2)
+            point = point_of_intersection(*lines)
+            ax = init_plot()
+            if point:
+                plot_points(ax, [point])
+            plot_lines(ax, lines)
+            export("{}/out/point_of_intersection_{}.png".format(WD, i))
 
     def demo_bst(n, s):
-        def f():
-            return randint(0, 50)
-
         seed(s)
-        tree = leaf(f())
-        for _ in range(n):
-            insert(tree, lt, leaf(f()))
+        xs = list(range(n))
+        shuffle(xs)
+        tree = leaf(xs[0])
+        for i in range(1, n):
+            insert(tree, lt, leaf(xs[i]))
         show(tree)
 
-    demo_convex_hulls(50)
-    demo_bst(15, 1)
+    # demo_convex_hulls(50)
+    demo_point_of_intersection(50)
+    # demo_bst(15, 0)
 
 
 if __name__ == "__main__":
