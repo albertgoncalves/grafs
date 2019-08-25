fcf2b1176061c614e4fb95486d1c4c152fe112c6
albertgoncalves <goncalves.albert@gmail.com>
2019-08-24 20:59:37 -0400

    starting from scratch

--- shell.nix
+++ shell.nix
@@ -1,27 +1,15 @@
 with import <nixpkgs> {};
 mkShell {
     buildInputs = [
-        (haskell.packages.ghc865.ghcWithPackages (pkgs: [
-            pkgs.containers
-            pkgs.hindent
-            pkgs.hlint
-            pkgs.hoogle
-            pkgs.HUnit
-            pkgs.psqueues
-            pkgs.vector
-        ]))
         (with ocaml-ng.ocamlPackages_4_07; [
             findlib
             ocaml
             ocp-indent
-            psq
         ])
         (python37.withPackages(ps: with ps; [
             flake8
             matplotlib
-            numpy
         ]))
-        rlwrap
         shellcheck
     ];
     shellHook = ''
