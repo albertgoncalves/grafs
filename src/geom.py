3f7f36a00399b5daf48cedf2a1f933d0e4aae81d
albertgoncalves <goncalves.albert@gmail.com>
2019-08-25 14:28:25 -0400

    more renaming

--- src/geom.py
+++ src/geom.py
@@ -8,20 +8,20 @@ def ccw(a, b, c):
     return ((cy - ay) * (bx - ax)) > ((by - ay) * (cx - ax))
 
 
+def intersect(ab, cd):
+    (a, b) = ab
+    (c, d) = cd
+    return (ccw(a, c, d) != ccw(b, c, d)) and (ccw(a, b, c) != ccw(a, b, d))
+
+
 def determinant(a, b):
     (ax, ay) = a
     (bx, by) = b
     return (ax * by) - (ay * bx)
 
 
-def intersection_bool(ab, cd):
-    (a, b) = ab
-    (c, d) = cd
-    return (ccw(a, c, d) != ccw(b, c, d)) and (ccw(a, b, c) != ccw(a, b, d))
-
-
-def segment_intersection(ab, cd):
-    if intersection_bool(ab, cd):
+def point_of_intersection(ab, cd):
+    if intersect(ab, cd):
         ((ax, ay), (bx, by)) = ab
         ((cx, cy), (dx, dy)) = cd
         xdelta = (ax - bx, cx - dx)
