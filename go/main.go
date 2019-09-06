b71aabc20a1346709d38f7384a9d654c3787be19
albertgoncalves <goncalves.albert@gmail.com>
2019-09-06 18:28:01 -0400

    condense constructors

--- go/main.go
+++ go/main.go
@@ -7,24 +7,12 @@ import (
 
 func main() {
     a := geom.Segment{
-        geom.Pair{
-            X: 0.0,
-            Y: 0.0,
-        },
-        geom.Pair{
-            X: 1.0,
-            Y: 1.0,
-        },
+        geom.Pair{0.0, 0.0},
+        geom.Pair{1.0, 1.0},
     }
     b := geom.Segment{
-        geom.Pair{
-            X: 0.0,
-            Y: 2.0,
-        },
-        geom.Pair{
-            X: 1.0,
-            Y: 0.0,
-        },
+        geom.Pair{0.0, 3.0},
+        geom.Pair{1.0, 0.0},
     }
     point, err := geom.PointOfIntersection(a, b)
     if err == nil {
