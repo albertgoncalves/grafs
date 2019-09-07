6c83ad04ca0a8e2afa1a6b999deade5818f2d326
albertgoncalves <goncalves.albert@gmail.com>
2019-09-07 11:33:44 -0400

    linting!

--- go/src/geom/geom.go
+++ go/src/geom/geom.go
@@ -39,22 +39,22 @@ func det3(a, b, c Triple) float64 {
 }
 
 func det4(a, b, c, d Quad) float64 {
-    var A float64 = a.X * det3(
+    A := a.X * det3(
         Triple{b.Y, b.Z, b.W},
         Triple{c.Y, c.Z, c.W},
         Triple{d.Y, d.Z, d.W},
     )
-    var B float64 = a.Y * det3(
+    B := a.Y * det3(
         Triple{b.X, b.Z, b.W},
         Triple{c.X, c.Z, c.W},
         Triple{d.X, d.Z, d.W},
     )
-    var C float64 = a.Z * det3(
+    C := a.Z * det3(
         Triple{b.X, b.Y, b.W},
         Triple{c.X, c.Y, c.W},
         Triple{d.X, d.Y, d.W},
     )
-    var D float64 = a.W * det3(
+    D := a.W * det3(
         Triple{b.X, b.Y, b.Z},
         Triple{c.X, c.Y, c.Z},
         Triple{d.X, d.Y, d.Z},
@@ -75,7 +75,7 @@ func PointOfIntersection(l, r Segment) (Pair, error) {
     if intersect(l, r) {
         xdelta := Pair{l.A.X - l.B.X, r.A.X - r.B.X}
         ydelta := Pair{l.A.Y - l.B.Y, r.A.Y - r.B.Y}
-        var denominator float64 = det2(xdelta, ydelta)
+        denominator := det2(xdelta, ydelta)
         if denominator != 0.0 {
             d := Pair{det2(l.A, l.B), det2(r.A, r.B)}
             x := det2(d, xdelta) / denominator
