3818f11378e60f403f3fc3e59882dfb07855f17e
albertgoncalves <goncalves.albert@gmail.com>
2019-09-08 11:31:52 -0400

    basic gonum plot in action!

--- go/main.go
+++ go/main.go
@@ -1,20 +1,89 @@
 package main
 
 import (
-    "fmt"
     "gen"
     "geom"
+    "gonum.org/v1/plot"
+    "gonum.org/v1/plot/plotter"
+    "gonum.org/v1/plot/vg"
+    "gonum.org/v1/plot/vg/draw"
+    "image/color"
+    "log"
     "math/rand"
 )
 
+func pairsToXYs(pairs []geom.Pair) plotter.XYs {
+    n := len(pairs)
+    xys := make(plotter.XYs, n)
+    for i := 0; i < n; i++ {
+        xys[i].X = pairs[i].X
+        xys[i].Y = pairs[i].Y
+    }
+    return xys
+}
+
+func segmentsToXYs(segments []geom.Segment) []plotter.XYs {
+    n := len(segments)
+    xys := make([]plotter.XYs, n)
+    for i := 0; i < n; i++ {
+        xy := make(plotter.XYs, 2)
+        xy[0].X = segments[i].A.X
+        xy[0].Y = segments[i].A.Y
+        xy[1].X = segments[i].B.X
+        xy[1].Y = segments[i].B.Y
+        xys[i] = xy
+    }
+    return xys
+}
+
+func randomUInt8() uint8 {
+    return uint8(rand.Intn(255))
+}
+
+func randomRGB() color.RGBA {
+    return color.RGBA{
+        R: randomUInt8(),
+        G: randomUInt8(),
+        B: randomUInt8(),
+        A: 255,
+    }
+}
+
+func addPairs(p *plot.Plot, pairs []geom.Pair) {
+    scatter, err := plotter.NewScatter(pairsToXYs(pairs))
+    scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+    scatter.Color = color.RGBA{R: 0, G: 0, B: 0, A: 200}
+    scatter.GlyphStyle.Radius = 5.0
+    if err != nil {
+        log.Panic(err)
+    }
+    p.Add(scatter)
+}
+
+func addSegments(p *plot.Plot, segments []geom.Segment) {
+    lines := segmentsToXYs(segments)
+    for _, line := range lines {
+        line, err := plotter.NewLine(line)
+        line.LineStyle.Color = randomRGB()
+        line.LineStyle.Width = 3.25
+        if err != nil {
+            log.Panic(err)
+        }
+        p.Add(line)
+    }
+}
+
 func main() {
-    rand.Seed(0)
-    a := gen.RandomSegment()
-    b := gen.RandomSegment()
-    point, err := geom.PointOfIntersection(a, b)
-    if err == nil {
-        fmt.Println(point)
-    } else {
-        fmt.Println(err)
+    rand.Seed(1)
+    n := 20
+    p, err := plot.New()
+    if err != nil {
+        log.Panic(err)
+    }
+    p.Add(plotter.NewGrid())
+    addSegments(p, gen.RandomSegments(n))
+    addPairs(p, gen.RandomPairs(n))
+    if err := p.Save(8*vg.Inch, 12*vg.Inch, "out/plot.png"); err != nil {
+        log.Panic(err)
     }
 }
