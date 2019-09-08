4cf5d5de24fb2e50164cdfa20242f9c765c8dbd3
albertgoncalves <goncalves.albert@gmail.com>
2019-09-08 12:10:39 -0400

    setup demo plotting, seems to work

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "flag"
+    "fmt"
     "gen"
     "geom"
     "gonum.org/v1/plot"
@@ -10,7 +12,12 @@ import (
     "image/color"
     "log"
     "math/rand"
+    "os"
 )
+
+const BOLD = "\033[1m"
+const END = "\033[0m"
+const TAB = "    "
 
 func pairsToXYs(pairs []geom.Pair) plotter.XYs {
     n := len(pairs)
@@ -49,11 +56,25 @@ func randomRGB() color.RGBA {
     }
 }
 
+func initPlot() *plot.Plot {
+    p, err := plot.New()
+    if err != nil {
+        log.Panic(err)
+    }
+    return p
+}
+
+func savePlot(p *plot.Plot, out string) {
+    if err := p.Save(8*vg.Inch, 12*vg.Inch, out); err != nil {
+        log.Panic(err)
+    }
+}
+
 func addPairs(p *plot.Plot, pairs []geom.Pair) {
     scatter, err := plotter.NewScatter(pairsToXYs(pairs))
     scatter.GlyphStyle.Shape = draw.CircleGlyph{}
     scatter.Color = color.RGBA{R: 0, G: 0, B: 0, A: 200}
-    scatter.GlyphStyle.Radius = 5.0
+    scatter.GlyphStyle.Radius = 4.25
     if err != nil {
         log.Panic(err)
     }
@@ -73,17 +94,36 @@ func addSegments(p *plot.Plot, segments []geom.Segment) {
     }
 }
 
+func flagProvided(name string) bool {
+    found := false
+    flag.Visit(func(f *flag.Flag) {
+        if f.Name == name {
+            found = true
+        }
+    })
+    return found
+}
+
 func main() {
-    rand.Seed(1)
-    n := 20
-    p, err := plot.New()
-    if err != nil {
-        log.Panic(err)
-    }
-    p.Add(plotter.NewGrid())
-    addSegments(p, gen.RandomSegments(n))
-    addPairs(p, gen.RandomPairs(n))
-    if err := p.Save(8*vg.Inch, 12*vg.Inch, "out/plot.png"); err != nil {
-        log.Panic(err)
+    demoSeed := flag.Int("d", 0, "seed")
+    flag.Parse()
+    if flagProvided("d") {
+        out := fmt.Sprintf("%s/out/demo.png", os.Getenv("GOPATH"))
+        fmt.Printf(
+            "$ %sDemo%s\n%sseed=%d\n%sout=%s\n",
+            BOLD,
+            END,
+            TAB,
+            *demoSeed,
+            TAB,
+            out,
+        )
+        rand.Seed(int64(*demoSeed))
+        n := 20
+        p := initPlot()
+        p.Add(plotter.NewGrid())
+        addSegments(p, gen.RandomSegments(n))
+        addPairs(p, gen.RandomPairs(n))
+        savePlot(p, out)
     }
 }
