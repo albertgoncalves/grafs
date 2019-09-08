9cde7dbebcbd80e35918518578c76a7a48adcc42
albertgoncalves <goncalves.albert@gmail.com>
2019-09-08 16:37:16 -0400

    slightly more concise

--- go/src/hull/hull.go
+++ go/src/hull/hull.go
@@ -12,28 +12,30 @@ func deleteIndex(pairs []geom.Pair, i int) []geom.Pair {
 }
 
 func ConvexHull(pairs []geom.Pair) ([]geom.Pair, []geom.Pair) {
+    var m int
     n := len(pairs)
-    sort.Sort(geom.PairByX(pairs))
+    sort.Slice(pairs, func(i, j int) bool {
+        if pairs[i].X == pairs[j].X {
+            return pairs[i].Y < pairs[j].Y
+        }
+        return pairs[i].X < pairs[j].X
+    })
     upper := pairs[:2]
     for i := 2; i < n; i++ {
         upper = append(upper, pairs[i])
-        for (len(upper) > 2) && !geom.Ccw(
-            upper[len(upper)-3],
-            upper[len(upper)-2],
-            upper[len(upper)-1],
-        ) {
-            upper = deleteIndex(upper, len(upper)-2)
+        m = len(upper)
+        for (m > 2) && !geom.Ccw(upper[m-3], upper[m-2], upper[m-1]) {
+            upper = deleteIndex(upper, m-2)
+            m = len(upper)
         }
     }
     lower := []geom.Pair{pairs[n-1], pairs[n-2]}
     for i := n - 2; -1 < i; i-- {
         lower = append(lower, pairs[i])
-        for (len(lower) > 2) && !geom.Ccw(
-            lower[len(lower)-3],
-            lower[len(lower)-2],
-            lower[len(lower)-1],
-        ) {
-            lower = deleteIndex(lower, len(lower)-2)
+        m = len(lower)
+        for (m > 2) && !geom.Ccw(lower[m-3], lower[m-2], lower[m-1]) {
+            lower = deleteIndex(lower, m-2)
+            m = len(lower)
         }
     }
     return upper, lower
